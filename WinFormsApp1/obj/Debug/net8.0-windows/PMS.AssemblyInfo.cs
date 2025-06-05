d93fbbec0934073d92ce60d2fbc5cb5d18edf4e0
d0t-J <j.ahmed.ja7a7@gmail.com>
2025-06-05 14:04:57 +0500

    Added AdminForm.cs

--- WinFormsApp1/obj/Debug/net8.0-windows/PMS.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/PMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645fcbcf255dfaaba5caf7663f4d8848b3aad724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03382cf8580c908a2b998ff1c6ef69facacea8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
